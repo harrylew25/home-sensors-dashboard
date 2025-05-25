39db9215d83a90b7bcdd07181a5b845a7165ebb9
Harry Lew <harrylew25@gmail.com>
2025-05-25 16:13:12 +0800

    Updated the mock data

--- app/mocked/handlers.ts
+++ app/mocked/handlers.ts
@@ -1,32 +1,37 @@
 import { http, HttpResponse } from "msw";
 
+const now = new Date();
 export const handlers = [
   http.get("/api/sensors/data", () => {
-    return HttpResponse.json([
-      {
-        id: 1,
-        name: "Temperature",
-        type: "temp",
-        value: "22°C",
-      },
-      {
-        id: 2,
-        name: "Humidity",
-        type: "humidity",
-        value: "45%",
-      },
-      {
-        id: 3,
-        name: "Energy Consumption",
-        type: "energy",
-        value: "1500 kWh",
-      },
-      {
-        id: 4,
-        name: "Dock Status",
-        type: "dock",
-        value: "Connected",
-      },
-    ]);
+    return HttpResponse.json({
+      message: "Sensor data fetched successfully",
+      time: now.toISOString(),
+      sensors: [
+        {
+          id: 1,
+          name: "Temperature",
+          type: "temp",
+          value: "100°C",
+        },
+        {
+          id: 2,
+          name: "Humidity",
+          type: "humidity",
+          value: "45%",
+        },
+        {
+          id: 3,
+          name: "Energy Consumption",
+          type: "energy",
+          value: "1500 kWh",
+        },
+        {
+          id: 4,
+          name: "Dock Status",
+          type: "dock",
+          value: "locked",
+        },
+      ],
+    });
   }),
 ];
