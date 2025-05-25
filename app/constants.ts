599f06c3ccad85605078fd4faed84e32d6990a0b
Harry Lew <harrylew25@gmail.com>
2025-05-25 16:14:56 +0800

    Add sensorTypes constant and refactor Home component structure

---
+++ app/constants.ts
@@ -0,0 +1,7 @@
+export const sensorTypes = [
+  { value: "all", label: "All" },
+  { value: "temp", label: "Temperature" },
+  { value: "humidity", label: "Humidity" },
+  { value: "energy", label: "Energy Consumption" },
+  { value: "dock", label: "Lock Status" },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,32 @@
-import Tile from "./components/Tile";
+"use client";
 
-// should fetch data from /api/sensors/data
-const item = {
-  timestamp: "2025-03-21T10:30:00Z",
-  sensors: [
-    { sensorId: "sensor_001", temperature: 42 },
-    { sensorId: "sensor_002", humidity: 18 },
-    { sensorId: "sensor_003", energy: 2100 },
-    { sensorId: "sensor_004", doorLockStatus: "locked" },
-  ],
-};
-console.log(item);
+import DropdownSelect from "./components/DropdownSelect";
+import RangeSelector from "./components/RangeSelector";
+import SensorPanel from "./components/SensorPanel";
 
-const exampleData = {
-  timeStamp: "2023-10-01T12:00:00Z",
-  sensors: [
-    { id: 1, type: "temperature", value: 31 },
-    { id: 2, type: "humidity", value: 70 },
-    { id: 3, type: "energy", value: 2000 },
-    { id: 4, type: "dockLockStatus", value: "locked" },
-  ],
-};
+import { store } from "@/lib/store";
+import { Provider } from "react-redux";
+
+if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
+  // This ensures the worker only runs in the browser
+  import("./mocked/browser").then(({ worker }) => {
+    worker.start();
+  });
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Home sensor dashboard Home Page</h1>
-      <div className="flex justify-center flex-wrap">
-        <Tile title="Temperature" subtitle="31°C" />
-        <Tile title="Humidity" subtitle="70" />
-        <Tile title="Energy Consumption" subtitle="2000W" />
-        <div>
-          <Tile title="Dock Lock Status" subtitle="Locked">
-            <p>Dock Lock Status: {exampleData.sensors[3].value}</p>
-          </Tile>
+    <Provider store={store()}>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="text-2xl m-5 text-purple-500">
+          Home sensor dashboard Home Page
+        </h1>
+        <div className="flex justify-center flex-wrap">
+          <RangeSelector />
+          <DropdownSelect />
         </div>
+        <SensorPanel />
       </div>
-      <p>Last updated: 2023-10-01</p>
-    </div>
+    </Provider>
   );
 }
