23b09a9e5aebb057c755876938cdd4527d33504a
Harry Lew <harrylew25@gmail.com>
2025-05-26 16:17:09 +0800

    Remove repeated functionality

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,7 @@ import DropdownSelect from "./components/DropdownSelect";
 import RangeSelector from "./components/RangeSelector";
 import SensorPanel from "./components/SensorPanel";
 
-import { store } from "@/lib/store";
-import { Provider } from "react-redux";
 import MessageAlert from "./components/MessageAlert";
-
-// if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
-//   // This ensures the worker only runs in the browser
-//   import("./mocked/browser").then(({ worker }) => {
-//     worker.start();
-//   });
-// }
 
 export default function Home() {
   const isOffline = useOffline();
@@ -25,19 +16,17 @@ export default function Home() {
   }
 
   return (
-    <Provider store={store}>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-2xl m-5 text-purple-500">
-          Home sensor dashboard Home Page
-        </h1>
-        {isOffline && <MessageAlert />}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-2xl m-5 text-purple-500">
+        Home sensor dashboard Home Page
+      </h1>
+      {isOffline && <MessageAlert />}
 
-        <div className="flex justify-center flex-wrap">
-          <RangeSelector />
-          <DropdownSelect />
-        </div>
-        <SensorPanel />
+      <div className="flex justify-center flex-wrap">
+        <RangeSelector />
+        <DropdownSelect />
       </div>
-    </Provider>
+      <SensorPanel />
+    </div>
   );
 }
